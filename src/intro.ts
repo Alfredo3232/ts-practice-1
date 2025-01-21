b34ee5224856ef8806b8195e2386680c1e181086
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2025-01-21 12:36:38 -0800

    Add comments to explain

--- src/intro.ts
+++ src/intro.ts
@@ -7,6 +7,7 @@ console.log("Alfredo");
 console.log(user.name);
 
 // The name parameter needs to be a string
+// We can obviously change this into boolean or number
 let testing = (name: string) => {
     return name.length;
 }
--- src/variableMe.ts
+++ src/variableMe.ts
@@ -1,31 +1,34 @@
+// string
 let greetings: string = "Hello Alfredo";
-
-// let mynum = 6;
-
-// mynum.toUpperCase();
-// greetings.toLocaleLowercase();
-
 // number
-let userId = 334455.3
-
+let userId: number = 334455.3;
 // boolean
 let isLoggedIn: boolean = false;
 
+// TypeScript can also infer what type is a variable, depending on its value so actually
+// putting :string is redundant
+let exampleStr = "I am being inferred";
 
 console.log(greetings);
+console.log(userId);
+console.log(isLoggedIn);
+
+console.log(exampleStr);
 
 
-// any
-let hero: string;
+// any - any value can be inserted - not optimal
+let hero: any;
 
 function getHero() {
     return "thor";
 }
 
 hero = getHero();
+// hover over getHero and we get string val
 
+// testing on import to see if typescript and check on different files
 import testing from "./intro";
-let name : number;
+let name: number;
 
 name = testing("alfredo");
 
